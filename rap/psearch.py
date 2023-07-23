49ea0f14410c1f46cc69df82d50cad7a0c3af53b
iquhsek <shuqike.research@gmail.com>
2023-07-22 21:52:08 -0400

    Updated reward

--- rap/psearch.py
+++ rap/psearch.py
@@ -82,7 +82,7 @@ class ReasoningITERSNode(ITERSNode):
 
     @property
     def reward(self):
-        return self._r0 * self._r_alpha + self._r1
+        return self._r0 * self._r_alpha + self._r1 if self.is_terminal else self._r0 * self._r_alpha
 
     def print(self, mcts, file=None):
         def pprint(*args):
