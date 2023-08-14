7f5f072546093a9318d2946b9ed44bfc02371553
iquhsek <shuqike.research@gmail.com>
2023-08-13 22:10:30 -0400

    Update success_stat.py
    debug

--- success_stat.py
+++ success_stat.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
                 line = f.readline()
                 num_success, num_sample = line.split(',')
                 total_success += bool(num_success)
+                print(num_success, bool(num_success)) #TODO: debug
     else:
         data_path = os.path.join('logs', args.run_name, 'json')
         sample_files = os.listdir(data_path)
