09d226410765e063431d0baabd8bf4a4f08104d6
iquhsek <shuqike.research@gmail.com>
2023-08-14 09:11:59 -0400

    Update plan_forward.py
    See prompts

--- rap/plan_forward.py
+++ rap/plan_forward.py
@@ -136,4 +136,13 @@ def forward_plan(initial_state: str,
         new_node, tmp_sample = planner(cur_node)
         tot_sample += tmp_sample
         cur_node = deepcopy(new_node)
+    # TODO:
+    if not cur_node.achieved_goal:
+        print()
+        print()
+        print('-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#')
+        print(cur_node.prompt)
+        print('-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#')
+        print()
+        print()
     return cur_node.achieved_goal, tot_sample
