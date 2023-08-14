2bbccfe744a433c054b2b5923a047d8d5e3ca85b
iquhsek <shuqike.research@gmail.com>
2023-08-14 00:09:38 -0400

    Update Aug13dmct.sh

--- Aug13dmct.sh
+++ Aug13dmct.sh
@@ -1,4 +1,4 @@
-rollouts=2
+rollouts=1
 CUDA_VISIBLE_DEVICES=0 nohup python -m torch.distributed.run --master_port 1034 --nproc_per_node 1 run_blocksworld.py --task mcts --model_name Vicuna --verbose False \
 --data data/blocksworld/step_4.json \
 --max_depth 4 \
@@ -6,4 +6,3 @@ CUDA_VISIBLE_DEVICES=0 nohup python -m torch.distributed.run --master_port 1034 
 --rollouts $rollouts \
 --model_path lmsys/vicuna-13b-v1.3 \
 --num_gpus 1 \
---single_run 0
